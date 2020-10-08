eea2f3190e5b5e07c2b05877d2c5edda4c4dcead
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2020-10-08 07:00:09 +0300

    [TASK-68] UIN Override changes (#69)
    * Update CHANGELOG.md
    
    * Remove the record when expiration - using MongoDB TTL feature.
    
    * Document the expiration index
    
    * Make sure that expired uin override is not given to the client(MongoDB TTL feature has a delay)
    
    * Fix warnings
    
    * Fix lint issue
    
    * No need to addd initial data for app versions anymore because this has been deployed to all enviroments.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- UIN Override changes. [#68](https://github.com/rokwire/health-building-block/issues/68)
 
 ## [1.25.0] - 2020-10-05
 ### Added
--- driven/storage/database.go
+++ driven/storage/database.go
@@ -637,6 +637,15 @@ func (m *database) applyUINOverridesChecks(uinoverrides *collectionWrapper) erro
 		return err
 	}
 
+	//add index - delete records when expiration - time to live 0
+	options := options.Index()
+	eas := int32(0)
+	options.ExpireAfterSeconds = &eas
+	err = uinoverrides.AddIndexWithOptions(bson.D{primitive.E{Key: "expiration", Value: 1}}, options)
+	if err != nil {
+		return err
+	}
+
 	log.Println("uinOverrides checks passed")
 	return nil
 }
@@ -661,27 +670,6 @@ func (m *database) applyAppVersionsChecks(appversions *collectionWrapper) error 
 	err := appversions.AddIndex(bson.D{primitive.E{Key: "version", Value: 1}}, true)
 	if err != nil {
 		return err
-	}
-
-	//check if need to add initial data
-	filter := bson.D{}
-	var result []bson.D
-	err = appversions.Find(filter, &result, nil)
-	if err != nil {
-		return err
-	}
-	if len(result) == 0 {
-		log.Println("need to add initial data, so adding it")
-
-		versions := []interface{}{bson.D{primitive.E{Key: "version", Value: "2.6"}},
-			bson.D{primitive.E{Key: "version", Value: "2.7"}},
-			bson.D{primitive.E{Key: "version", Value: "2.8"}}}
-		_, err = appversions.InsertMany(versions, nil)
-		if err != nil {
-			return err
-		}
-	} else {
-		log.Println("no need to add initial data")
 	}
 
 	log.Println("appVersions checks passed")

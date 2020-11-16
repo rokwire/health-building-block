69d1b8fff0ce73841260074db31b4d863425884b
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2020-11-16 12:43:29 +0200

    Update the doc

--- docs/docs.go
+++ docs/docs.go
@@ -7471,7 +7471,7 @@ type swaggerInfo struct {
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
-	Version:     "1.30.0",
+	Version:     "1.31.0",
 	Host:        "localhost",
 	BasePath:    "/health",
 	Schemes:     []string{"https"},

67a7204985616190f6a968b4c65607849d199c2c
Petyo Stoyanov <petyo.st.stoyanov@gmail.com>
2020-12-02 09:16:56 +0200

    Change the construct filter data function name

--- driven/storage/adapter.go
+++ driven/storage/adapter.go
@@ -1426,7 +1426,7 @@ func (sa *Adapter) FindCounties(f *utils.Filter) ([]*model.County, error) {
 	//add filter
 	var filter interface{}
 	if f != nil {
-		filter = constructFilter(f)
+		filter = constructDataFilter(f)
 	}
 
 	var result []*county
@@ -4396,7 +4396,7 @@ func (sa *Adapter) FindRosterByPhone(phone string) (map[string]string, error) {
 func (sa *Adapter) FindRosters(f *utils.Filter, sortBy string, sortOrder int, limit int, offset int) ([]map[string]interface{}, error) {
 	var filter bson.D
 	if f != nil {
-		filter = constructFilter(f).(bson.D)
+		filter = constructDataFilter(f).(bson.D)
 	}
 
 	options := options.Find()
@@ -4666,7 +4666,7 @@ func NewStorageAdapter(mongoDBAuth string, mongoDBName string, mongoTimeout stri
 	return &Adapter{db: db}
 }
 
-func constructFilter(f *utils.Filter) interface{} {
+func constructDataFilter(f *utils.Filter) interface{} {
 	if f == nil || len(f.Items) == 0 {
 		return bson.D{}
 	}
